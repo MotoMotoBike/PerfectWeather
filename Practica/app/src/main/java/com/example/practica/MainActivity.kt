b2b1ad3e4c59479c7483d4733b6619256098fa65
MotoMotoBike <kazanin-roman@mail.ru>
2020-11-10 16:56:11 +0300

    Bot + выпадающий список

--- Practica/app/src/main/java/com/example/practica/MainActivity.kt
+++ Practica/app/src/main/java/com/example/practica/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.practica
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_main.*
+
+const val URL = "api.openweathermap.org/data/2.5/weather?q="
+const val APIKEY = "bc06456dbcab3bbd3e95b93b559691eb"
+
+var cities = arrayOf("Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
