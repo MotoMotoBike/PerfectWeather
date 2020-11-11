9aa81da1ef4b6e8b18b73f8ca0ac1c92e5d54ce4
Andrey Komarov <dronkomarov@gmail.com>
2020-11-11 15:17:09 +0300

    Cities spinner fix

--- Practica/app/src/main/java/com/example/practica/Fragment_main.kt
+++ Practica/app/src/main/java/com/example/practica/Fragment_main.kt
@@ -5,6 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import androidx.core.view.get
+import kotlinx.android.synthetic.main.fragment_main.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -16,6 +20,10 @@ private const val ARG_PARAM2 = "param2"
  * Use the [Fragment_main.newInstance] factory method to
  * create an instance of this fragment.
  */
+
+var Citys = arrayOf("Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж")
+
+
 class Fragment_main : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -35,6 +43,25 @@ class Fragment_main : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        val arrayAdapter =
+            activity?.let { ArrayAdapter (it, android.R.layout.simple_spinner_item, Citys) }
+
+        cities.adapter = arrayAdapter
+
+//        cities.onItemSelectedListener = object :
+//            AdapterView.OnItemSelectedListener{
+//            override fun onNothingSelected(parent: AdapterView<*>?) {
+//                TODO("Not yet implemented")
+//            }
+//
+//            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+//                asd.text = cities[position].toString()
+//            }
+//        }
     }
 
     companion object {
--- Practica/app/src/main/java/com/example/practica/MainActivity.kt
+++ Practica/app/src/main/java/com/example/practica/MainActivity.kt
@@ -14,7 +14,6 @@ import kotlinx.android.synthetic.main.fragment_main.*
 const val URL = "api.openweathermap.org/data/2.5/weather?q="
 const val APIKEY = "bc06456dbcab3bbd3e95b93b559691eb"
 
-var Citys = arrayOf("Москва", "Самара", "Вологда", "Волгоград", "Саратов", "Воронеж")
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,19 +23,6 @@ class MainActivity : AppCompatActivity() {
         val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
         NavigationUI.setupWithNavController(bottomNavigationView, navController)
 
-        val arrayAdapter = ArrayAdapter (this, android.R.layout.simple_spinner_item, Citys)
 
-        cities.adapter = arrayAdapter
-
-        cities.onItemSelectedListener = object :
-            AdapterView.OnItemSelectedListener{
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                TODO("Not yet implemented")
-            }
-
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                asd.text = cities[position].toString()
-            }
-        }
     }
 }
